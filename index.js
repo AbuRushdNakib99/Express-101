52665842b8602d038f07e5a2e2d11c901d4249b9
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2022-07-05 20:54:53 +0600

    send a html file by response object

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express')
 
 const app=express();
 
+app.get("/",(req,res)=>{
+    res.send("<h1>This is a Express js File</h1>")
+})
+
 app.listen(4000,()=>{
     console.log("Server is listening");
 })
